2ec5c1685ec216894cc4651b9d34cc9462539640
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-09-11 01:26:01 -0400

    add folders and file template

---
+++ dir_{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}.sql
@@ -0,0 +1,2 @@
+﻿CREATE SCHEMA {{cookiecutter.schema_name}} AUTHORIZATION dbo;
+EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'{{cookiecutter.schema_name}}';
