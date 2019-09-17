8241aaacc4f0309f25abe696ba8292bcf091e5de
sdoubleday <28907224+sdoubleday@users.noreply.github.com>
2019-09-16 20:31:20 -0400

    Update {{cookiecutter.schema_name}}.sql
    Add batch separator

--- dir_{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}.sql
+++ dir_{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}/{{cookiecutter.schema_name}}.sql
@@ -1,2 +1,4 @@
 ﻿CREATE SCHEMA {{cookiecutter.schema_name}} AUTHORIZATION dbo;
+GO
 EXECUTE sp_addextendedproperty @name = N'tSQLt.TestClass', @value = 1, @level0type = N'SCHEMA', @level0name = N'{{cookiecutter.schema_name}}';
+GO
